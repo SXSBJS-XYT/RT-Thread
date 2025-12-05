0964d1126290109e8abe29918c2aaee45e97720a
GitCode_Xyt <1728846899@qq.com>
2025-12-05 15:30:46 +0800

    <update>更新 app_task.c 代码格式

--- Kernel/2.UartPrintf/rt_kprintf/Application/Src/app_task.c
+++ Kernel/2.UartPrintf/rt_kprintf/Application/Src/app_task.c
@@ -13,18 +13,18 @@ extern rt_uint32_t cnt;
 static void printf_thread_entry(void *param)
 {
 
-    while (1)
-    {
-        rt_kprintf("cnt = %d", cnt);
-        rt_thread_mdelay(500);
-    }
+  while (1)
+  {
+    rt_kprintf("cnt = %d", cnt);
+    rt_thread_mdelay(500);
+  }
 }
 
 /* 任务初始化函数 */
 void app_task_init(void)
 {
-    /* 创建线程 */
-    rt_thread_init(&printf_thread, "printf", printf_thread_entry, RT_NULL, printf_stack,
-                   sizeof(printf_stack), 20, 10);
-    rt_thread_startup(&printf_thread);
+  /* 创建线程 */
+  rt_thread_init(&printf_thread, "printf", printf_thread_entry, RT_NULL, printf_stack,
+                 sizeof(printf_stack), 20, 10);
+  rt_thread_startup(&printf_thread);
 }
